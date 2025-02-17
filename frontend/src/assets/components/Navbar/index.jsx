dac7133a317630cc78ae6807c1fa09b61965ae9e
joaovjm <joao.oliveira18.jm@gmail.com>
2025-02-17 00:54:37 -0300

    Increased menu dropdown and style

--- frontend/src/assets/components/Navbar/index.jsx
+++ frontend/src/assets/components/Navbar/index.jsx
@@ -1,20 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router";
 import "./navbar.css";
 import { MdOutlineLogin } from "react-icons/md";
+import { AdminMenu, Navitens, OperadorMenu, RelatórioMenu } from "../Navitens";
 
 const Navbar = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const [showDropdown, setShowDropdown] = useState(null);
+
   return (
     <>
-      <header >
+      <header>
         <nav className="nav">
-          <Link to="/" className="logo">
-            Logo
+          <Link to="/">
+            logo
           </Link>
-          <Link to="/login" className="link_login">
-            <MdOutlineLogin />
-            Login
-          </Link>
+
+          {!isAuthenticated ? (
+            <Link to="/login" className="link_login">
+              <MdOutlineLogin />
+              Login
+            </Link>
+          ) : (
+            <ul className="nav-items">
+              {Navitens.map((item) => (
+                <li
+                  key={item.id}
+                  className={item.cName}
+                  onMouseEnter={() => setShowDropdown(item.title)}
+                  onMouseLeave={() => setShowDropdown(null)}
+                >
+                  <p>{item.title}</p>
+
+                  {/* Dropdown Sob-menu AdminMenu */}
+
+                  {item.title === "Admin" && showDropdown === "Admin" && (
+                    <ul className="dropdown-admin">
+                      {AdminMenu.map((admin) => (
+                        <li key={admin.id} className={admin.cName}>
+                          <Link to={admin.path}>{admin.title}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+
+                  {/* Dropdown Sob-menu RelatórioMenu */}
+
+                  {item.title === "Relatório" && showDropdown == "Relatório" && (
+                    <ul className="dropdown-admin">
+                      {RelatórioMenu.map((admin) => (
+                        <li key={admin.id} className={admin.cName}>
+                          <Link to={admin.path}>{admin.title}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+
+                  {/* Dropdown Sob-menu OperadorMenu */}
+
+                  {item.title === "Operador" && showDropdown === "Operador" && (
+                    <ul className="dropdown-admin">
+                      {OperadorMenu.map((admin) => (
+                        <li key={admin.id} className={admin.cName}>
+                          <Link to={admin.path}>{admin.title}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              ))}
+            </ul>
+          )}
         </nav>
       </header>
       <Outlet />
