f8b046ad56ee034b959a047c97cb54f234a863b4
joaovjm <joao.oliveira18.jm@gmail.com>
2025-02-17 00:55:29 -0300

    Created Navitems

---
+++ frontend/src/assets/components/Navitens/index.jsx
@@ -0,0 +1,104 @@
+export const Navitens = [
+    {
+        id: 1,
+        title: "Admin",
+        cName: "nav-item",
+    },
+    {
+        id: 2,
+        title: "Relatório",
+        cName: "nav-item",
+    },
+    {
+        id: 3,
+        title: "Operador",
+        cName: "nav-item",
+    },
+];
+
+export const AdminMenu = [
+    {
+        id: 1,
+        title: "Receber Doação",
+        path: "./receberdoacao",
+        cName: "submenu-itens",
+    },
+    {
+        id: 2,
+        title: "Mudar Coletador",
+        path: "./mudarcoletador",
+        cName: "submenu-itens",
+    },
+    {
+        id: 3,
+        title: "Verificar Impressão",
+        path: "./verificarimpressao",
+        cName: "submenu-itens",
+    },
+    {
+        id: 4,
+        title: "Areas a Imprimir",
+        path: "./areasaimprimir",
+        cName: "submenu-itens",
+    },
+    {
+        id: 5,
+        title: "Criar Doação Mensal",
+        path: "./criardoadormensal",
+        cName: "submenu-itens",
+    },
+    {
+        id: 6,
+        title: "Operadores",
+        path: "./operadores",
+        cName: "submenu-itens",
+    },
+];
+
+export const RelatórioMenu = [
+    {
+       id: 1,
+       title: "Doação Recebida",
+       path: "./doacaorecebida",      
+       cName: "submenu-itens",
+    },
+    {
+        id: 2,
+        title:"Trabalho do Operador",
+        path: "./trabalhooperador",
+        cName: "submenu-itens",
+    },
+    {
+        id: 3,
+        title:"Histórico de Trabalho",
+        path: "./historicodetrabalho",
+        cName: "submenu-itens",
+    },
+    {
+        id: 4,
+        title:"Movimento dos Coletadores",
+        path: "./movimentocoletadores",
+        cName: "submenu-itens",
+    },
+    {
+        id: 5,
+        title:"Contar Doações",
+        path: "./contardoador",
+        cName: "submenu-itens",
+    }
+];
+
+export const OperadorMenu = [
+    {
+        id: 1,
+        title: "Buscar doador",
+        path: "./searchdonor",
+        cName: "submenu-itens",
+    },
+    {
+        id: 2,
+        title: "Lista de Trabalho",
+        path: "./listadetrabalho",
+        cName: "submenu-itens",
+    }
+];
