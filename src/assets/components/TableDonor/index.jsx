f51117913319f644990c6ba435310519f4da02d1
joaovjm <joao.oliveira18.jm@gmail.com>
2025-03-04 14:15:21 -0300

    Integrated new database

--- src/assets/components/TableDonor/index.jsx
+++ src/assets/components/TableDonor/index.jsx
@@ -4,6 +4,8 @@ import { getDonations } from "../../../helper/supabase";
 
 const TableDonor = ({ idDonor }) => {
   const [dados, setDados] = useState([]);
+
+  const media = (dados.reduce((acc, curr) => acc + curr.valor, 0) / dados.length).toFixed(2);
   useEffect(() => {
     if (idDonor) {
       getDonations(idDonor)
@@ -19,36 +21,41 @@ const TableDonor = ({ idDonor }) => {
   return (
     <table border="1" className="tabledonor">
       <thead>
-        <tr>
-          <th>Recibo</th>
-          <th>Operador</th>
-          <th>Valor</th>
-          <th>Contato</th>
-          <th>Receber</th>
-          <th>Recebida</th>
-          <th>Impresso</th>
-          <th>Recebido</th>
-          <th>MesRef</th>
-          <th>Coletador</th>
+        <tr className="trHead">
+          <th className="tableHead">Recibo</th>
+          <th className="tableHead">Operador</th>
+          <th className="tableHead">Valor</th>
+          <th className="tableHead">Comissão</th>
+          <th className="tableHead">Contato</th>
+          <th className="tableHead">Receber</th>
+          <th className="tableHead">Recebida</th>
+          <th className="tableHead">Impresso</th>
+          <th className="tableHead">Recebido</th>
+          <th className="tableHead">MesRef</th>
+          <th className="tableHead">Coletador</th>
         </tr>
       </thead>
 
-      <tbody>
-        {dados?.map((item) => (
-          <tr key={item.recibo}>
-            <td>{item.recibo}</td>
-            <td>{item.operador}</td>
-            <td>{item.valor}</td>
-            <td>{item.contato}</td>
-            <td>{item.receber}</td>
-            <td>{item.recebida}</td>
-            <td>{item.impresso}</td>
-            <td>{item.recebido}</td>
-            <td>{item.mesref}</td>
-            <td>{item.coletador}</td>
-          </tr>
-        ))}
-      </tbody>
+      {dados?.map((item) => (
+          <tbody>
+            <tr className="trBody" key={item.recibo}>
+              <td className="tableBody">{item.recibo}</td>
+              <td className="tableBody">{item.operador}</td>
+              <td className="tableBody">{item.valor}</td>
+              <td className="tableBody">{item.comissao}</td>
+              <td className="tableBody">{item.data_contato}</td>
+              <td className="tableBody">{item.receber}</td>
+              <td className="tableBody">{item.data_recebida}</td>
+              <td className="tableBody">{item.impresso}</td>
+              <td className="tableBody">{item.recebido}</td>
+              <td className="tableBody">{item.mesref}</td>
+              <td className="tableBody">{item.coletador}</td>
+            </tr>
+            <tr className="trFoot">
+              <td colSpan="11" className="obs">{item.obs_doacao ? item.obs_doacao : "..."}</td>
+            </tr>
+          </tbody>
+      ))}
     </table>
   );
 };
