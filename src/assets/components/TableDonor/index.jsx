80a051460d5622aca3b4bf5feca721e7a5c55489
joaovjm <joao.oliveira18.jm@gmail.com>
2025-03-05 10:34:46 -0300

    added responsiveness to the table on mobile devices

--- src/assets/components/TableDonor/index.jsx
+++ src/assets/components/TableDonor/index.jsx
@@ -5,7 +5,9 @@ import { getDonations } from "../../../helper/supabase";
 const TableDonor = ({ idDonor }) => {
   const [dados, setDados] = useState([]);
 
-  const media = (dados.reduce((acc, curr) => acc + curr.valor, 0) / dados.length).toFixed(2);
+  const media = (
+    dados.reduce((acc, curr) => acc + curr.valor, 0) / dados.length
+  ).toFixed(2);
   useEffect(() => {
     if (idDonor) {
       getDonations(idDonor)
@@ -19,44 +21,70 @@ const TableDonor = ({ idDonor }) => {
   }, [idDonor]);
 
   return (
-    <table border="1" className="tabledonor">
-      <thead>
-        <tr className="trHead">
-          <th className="tableHead">Recibo</th>
-          <th className="tableHead">Operador</th>
-          <th className="tableHead">Valor</th>
-          <th className="tableHead">Comissão</th>
-          <th className="tableHead">Contato</th>
-          <th className="tableHead">Receber</th>
-          <th className="tableHead">Recebida</th>
-          <th className="tableHead">Impresso</th>
-          <th className="tableHead">Recebido</th>
-          <th className="tableHead">MesRef</th>
-          <th className="tableHead">Coletador</th>
-        </tr>
-      </thead>
+    <div className="table-container">
+      <table border="1" className="tabledonor">
+        <thead>
+          <tr className="trHead">
+            <th className="tableHead">Recibo</th>
+            <th className="tableHead">Operador</th>
+            <th className="tableHead">Valor</th>
+            <th className="tableHead">Comissão</th>
+            <th className="tableHead">Contato</th>
+            <th className="tableHead">Receber</th>
+            <th className="tableHead">Recebida</th>
+            <th className="tableHead">Impresso</th>
+            <th className="tableHead">Recebido</th>
+            <th className="tableHead">MesRef</th>
+            <th className="tableHead">Coletador</th>
+          </tr>
+        </thead>
 
-      {dados?.map((item) => (
+        {dados?.map((item) => (
           <tbody>
             <tr className="trBody" key={item.recibo}>
-              <td className="tableBody">{item.recibo}</td>
-              <td className="tableBody">{item.operador}</td>
-              <td className="tableBody">{item.valor}</td>
-              <td className="tableBody">{item.comissao}</td>
-              <td className="tableBody">{item.data_contato}</td>
-              <td className="tableBody">{item.receber}</td>
-              <td className="tableBody">{item.data_recebida}</td>
-              <td className="tableBody">{item.impresso}</td>
-              <td className="tableBody">{item.recebido}</td>
-              <td className="tableBody">{item.mesref}</td>
-              <td className="tableBody">{item.coletador}</td>
+              <td data-label="Recibo" className="tableBody">
+                {item.recibo}
+              </td>
+              <td data-label="Operador" className="tableBody">
+                {item.operador}
+              </td>
+              <td data-label="Valor" className="tableBody">
+                {item.valor}
+              </td>
+              <td data-label="Comissão" className="tableBody">
+                {item.comissao}
+              </td>
+              <td data-label="Contato" className="tableBody">
+                {item.data_contato}
+              </td>
+              <td data-label="Receber" className="tableBody">
+                {item.receber}
+              </td>
+              <td data-label="Recebida" className="tableBody">
+                {item.data_recebida}
+              </td>
+              <td data-label="Impresso" className="tableBody">
+                {item.impresso}
+              </td>
+              <td data-label="Recebido" className="tableBody">
+                {item.recebido}
+              </td>
+              <td data-label="MesRef" className="tableBody">
+                {item.mesref}
+              </td>
+              <td data-label="Coletador" className="tableBody">
+                {item.coletador}
+              </td>
             </tr>
             <tr className="trFoot">
-              <td colSpan="11" className="obs">{item.obs_doacao ? item.obs_doacao : "..."}</td>
+              <td colSpan="11" className="obs">
+                {item.obs_doacao ? item.obs_doacao : "..."}
+              </td>
             </tr>
           </tbody>
-      ))}
-    </table>
+        ))}
+      </table>
+    </div>
   );
 };
 
