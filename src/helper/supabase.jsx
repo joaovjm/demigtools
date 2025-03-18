cfd4fe02d28dd267a3a2d889748cbc6a7c3a9a63
joaovjm <joao.oliveira18.jm@gmail.com>
2025-03-17 22:43:19 -0300

    update supabase

--- src/helper/supabase.jsx
+++ src/helper/supabase.jsx
@@ -121,6 +121,21 @@ export const getDonations = async (idDonor) => {
   .from("doação")
   .select()
   .eq("id_doacao_doador", idDonor)
-  console.log(data)
   return data
+}
+
+export const insertDonation = async (valor, data, descricao, idDonor, comissao, data_receber) => {
+  const {error} = await supabase.from("doação").insert([{
+    valor: valor,
+    data_contato: data,
+    obs_doacao: descricao,
+    id_doacao_doador: idDonor,
+    comissao: comissao,
+    data_receber: data_receber
+  }])
+  if (!error) {
+    window.alert("Doação criada com sucesso!")
+  } else {
+    window.alert("Erro ao criar doação: ", error)
+  }
 }
