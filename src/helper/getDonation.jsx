be0a838e86f30d18623af5735cdd9806183709e0
joaovjm <joao.oliveira18.jm@gmail.com>
2025-03-20 11:22:21 -0300

    Create donation api

---
+++ src/helper/getDonation.jsx
@@ -0,0 +1,27 @@
+import supabase from "./superBaseClient"
+
+export const getDonation = async (donor_id) => {
+    try{
+        const {data, error} = await supabase.from("donation").select(`
+            receipt_donation_id,
+            donation_value,
+            donation_extra,
+            donation_day_contact,
+            donation_day_to_receive,
+            donation_day_received,
+            donation_print,
+            donation_received,
+            donation_monthref,
+            donation_description    
+        `).eq("donor_id", donor_id)
+
+        if (error) throw error
+        return data
+    } catch (error) {
+        console.error("Erro ao buscar os dados: ", error.message)
+    }
+        
+    
+
+    
+}
