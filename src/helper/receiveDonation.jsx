fffe5a91ce289ad8cebd001f27fc19f891c0b43e
joaovjm <joao.oliveira18.jm@gmail.com>
2025-03-24 21:50:46 -0300

    Created receiveDonation

---
+++ src/helper/receiveDonation.jsx
@@ -0,0 +1,62 @@
+import supabase from "./superBaseClient";
+
+export const receiveDonation = async (
+  date,
+  setMessage,
+  collector,
+  setTypeAlert,
+  search,
+  setTableReceipt
+) => {
+
+  if (date !== "" && collector !== "") {
+
+    //Busca do Nome do Doador
+    try {
+      const { data, error } = await supabase
+        .from("donation")
+        .select(
+          `     donation_value,
+                donor:donor_id (donor_name)`
+        )
+        .eq("receipt_donation_id", search);
+
+        if (error) throw error;
+
+        if (data.length > 0) {
+            const { donation_value, donor} = data[0];
+            const name = donor?.donor_name;
+            const value = donation_value;
+    
+            const {error: updateError} = await supabase.from("donation").update({
+                donation_received: "Sim",
+                donation_day_received: date,
+                collector_code_id: collector,
+            }).eq("receipt_donation_id", search);
+
+            if (updateError) throw updateError;
+
+            const newItem = {search, name, value};
+            setTableReceipt((prev) => [...prev, newItem]);
+            setMessage("Doações recebidas com sucesso!");
+            setTypeAlert("green")
+        } else {
+            setMessage("Recibo não localizado");
+            setTypeAlert("#940000");
+        }
+      
+    } catch (error) {
+        console.error("Erro: ", error.message);
+    }
+
+  } else {
+    setMessage(
+      "Os campos coletador, data e recibo, precisam ser preenchidos corretamente."
+    );
+    setTypeAlert("#F25205");
+  }
+
+  setTimeout(() => {
+    setMessage("");
+  }, 1000);
+};
