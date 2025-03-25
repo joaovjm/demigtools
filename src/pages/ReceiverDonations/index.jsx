b8f5d76bc703b72676bbf0418c1843ff1665c9d6
joaovjm <joao.oliveira18.jm@gmail.com>
2025-03-24 21:49:31 -0300

    Add functionaly in table and integrated page in database

--- src/pages/ReceiverDonations/index.jsx
+++ src/pages/ReceiverDonations/index.jsx
@@ -1,81 +1,128 @@
-import React, { useState } from 'react'
-import "./index.css"
-
+import React, { useEffect, useState } from "react";
+import "./index.css";
 import { FaMoneyCheckDollar } from "react-icons/fa6";
 import { GiConfirmed } from "react-icons/gi";
+import { getCollector } from "../../helper/getCollector";
+import { receiveDonation } from "../../helper/receiveDonation";
+import { GoAlertFill } from "react-icons/go";
+import { BiSolidErrorAlt } from "react-icons/bi";
 
 const ReceiverDonations = () => {
-  const [collector, setCollector] = useState('')
-  const [date, setDate] = useState('')
-  const [search, setSearch] = useState('')
-  const [message, setMessage] = useState('')
+  const [collector, setCollector] = useState("");
+  const [date, setDate] = useState("");
+  const [search, setSearch] = useState("");
+  const [message, setMessage] = useState("");
+  const [collectors, setCollectors] = useState([]);
+  const [tableReceipt, setTableReceipt] = useState([]);
+  const [typeAlert, setTypeAlert] = useState("");
 
   const handleReceiverDonations = (e) => {
-    e.preventDefault()
-    setMessage('Doações recebidas com sucesso!')
-    const messageTimeOut = setTimeout(() => {
-      setMessage('')
-    }, 1000)
-    return () => clearTimeout(messageTimeOut)
-  }
-
-
+    e.preventDefault();
+    receiveDonation(
+      date,
+      setMessage,
+      collector,
+      setTypeAlert,
+      search,
+      setTableReceipt
+    );
+    console.log(tableReceipt);
+  };
+  useEffect(() => {
+    getCollector().then((data) => {
+      setCollectors(data);
+    });
+  }, []);
   return (
-    <main className='receiver-donations-main'>
-      <div className='receiver-donations-header'>
+    <main className="receiver-donations-main">
+      <div className="receiver-donations-header">
         <div>
-          <h2 className='receiver-donations-header-title-text'><FaMoneyCheckDollar /> Receber Doações</h2>
+          <h2 className="receiver-donations-header-title-text">
+            <FaMoneyCheckDollar /> Receber Doações
+          </h2>
         </div>
       </div>
 
-      <form onSubmit={handleReceiverDonations} className='receiver-donations-form'>
-        <div className='receiver-donations-form-input'>
-          <label  className="label">Coletador</label>
-          <select value={collector} onChange={(e) => setCollector(e.target.value)}>
-            <option value="" disabled>Selecione o coletador</option>
-            <option value="João Oliveira">João Oliveira</option>
-            <option value="Jozinei Venancio">Jozinei Venancio</option>
-            <option value="Leandro Amoedo">Leandro Amoedo</option>
+      <form
+        onSubmit={handleReceiverDonations}
+        className="receiver-donations-form"
+      >
+        <div className="receiver-donations-form-input">
+          <label className="label">Coletador</label>
+          <select
+            value={collector}
+            onChange={(e) => setCollector(e.target.value)}
+          >
+            <option value="" disabled>
+              Selecione o coletador...
+            </option>
+            {collectors?.map((item) => (
+              <option
+                key={item.collector_code_id}
+                value={item.collector_code_id}
+              >
+                {item.collector_name}
+              </option>
+            ))}
           </select>
         </div>
 
-        <div className='receiver-donations-form-input'>
-          <label  className="label">Data</label>
-          <input type='date' value={date} onChange={(e) => setDate(e.target.value)}/>
+        <div className="receiver-donations-form-input">
+          <label className="label">Data</label>
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
         </div>
 
-        <div className='receiver-donations-form-input'>
-          <label  className="label">Buscar</label>
-          <input type='text' value={search} onChange={(e) => setSearch(e.target.value)}/>
+        <div className="receiver-donations-form-input">
+          <label className="label">Buscar</label>
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </div>
       </form>
 
-      <div className='receiver-donations-form-message'>
+      {/*MENSAGEM*/}
+      <div className="receiver-donations-form-message">
         {message && (
-          <div style={{backgroundColor: 'green', width: '100%'}} className='receiver-donations-form-message'>
-            <p className='receiver-donations-form-message-text'>{message}<GiConfirmed /></p>
+          <div
+            style={{ backgroundColor: typeAlert, width: "100%" }}
+            className="receiver-donations-form-message"
+          >
+            <p className="receiver-donations-form-message-text">
+              {message}
+              {typeAlert === "green" && <GiConfirmed />}
+              {typeAlert === "#940000" && <GoAlertFill />}
+              {typeAlert === "#F25205" && <BiSolidErrorAlt />}
+            </p>
           </div>
         )}
       </div>
 
-      <table className='receiver-donations-table'>
-        <thead className='receiver-donations-table-header'>
+      <table className="receiver-donations-table">
+        <thead className="receiver-donations-table-header">
           <tr>
-            <th style={{width: '20%'}}>Recibo</th>
-            <th style={{width: '60%'}}>Nome</th>
-            <th style={{width: '20%'}}>Valor</th>
+            <th style={{ width: "20%" }}>Recibo</th>
+            <th style={{ width: "60%" }}>Nome</th>
+            <th style={{ width: "20%" }}>Valor</th>
           </tr>
         </thead>
-        <tbody className='receiver-donations-table-body'>
-          <tr>
-            <td>123458</td>
-            <td>João Marcos Oliveira da Silva</td>
-            <td>50</td>
-          </tr>
+        <tbody className="receiver-donations-table-body">
+          {tableReceipt.map((item) => (
+            <tr key={item.search}>
+              <td>{item.search}</td>
+              <td>{item.name}</td>
+              <td>{item.value}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </main>
-  )
-}
+  );
+};
 
-export default ReceiverDonations
+export default ReceiverDonations;
