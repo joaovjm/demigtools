139c0ea4ee89bc51ce7f04a2156dfa868d571c5a
joaovjm <joao.oliveira18.jm@gmail.com>
2025-03-27 01:07:37 -0300

    Modified

--- src/helper/changeCollector.jsx
+++ src/helper/changeCollector.jsx
@@ -1,7 +1,23 @@
 import supabase from "./superBaseClient"
 
-export const changeCollector = async (collector_code_id, receipt_donation_id) => {
-    await supabase.from("donation").update([{
-        collector_code_id: collector_code_id
-    }]).eq("receipt_donation_id", receipt_donation_id)
+export const changeCollector = async (collector_code_id, receipt_donation_id, dateFormat) => {
+    try{
+        const { data, error } = await supabase.from("donation").select("donation_received").eq("receipt_donation_id", receipt_donation_id)
+
+        if (error) throw error
+
+        if (data[0].donation_received === "Não") {
+            const {data, error} = await supabase.from("donation").update([{
+                collector_code_id: collector_code_id,
+                donation_day_to_receive: dateFormat
+            }]).eq("receipt_donation_id", receipt_donation_id).select()
+    
+            return "Ok"
+        } else {
+            return "Sim"
+        }
+    } catch (error) {
+        return 0
+    }
+    
 }
