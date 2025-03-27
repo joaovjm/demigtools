7844e550503a49cc9aaa248f01d4a9bd32941da7
joaovjm <joao.oliveira18.jm@gmail.com>
2025-03-27 20:10:48 -0300

    Modified for adapt new modal

--- src/pages/ReceiverDonations/index.jsx
+++ src/pages/ReceiverDonations/index.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./index.css";
+
 import { FaMoneyCheckDollar } from "react-icons/fa6";
 import { GiConfirmed } from "react-icons/gi";
-import { getCollector } from "../../helper/getCollector";
-import { receiveDonation } from "../../helper/receiveDonation";
 import { GoAlertFill } from "react-icons/go";
 import { BiSolidErrorAlt } from "react-icons/bi";
+
+import { useDonation } from "../../helper/receiveDonation";
+import { getCollector } from "../../helper/getCollector";
 import { DataSelect } from "../../assets/components/DataTime";
-import { verifyCollector } from "../../helper/verifyCollector";
+import { ModalConfirm } from "../../assets/components/ModalConfirm";
 
 const ReceiverDonations = () => {
   const [collector, setCollector] = useState("");
@@ -18,30 +20,12 @@ const ReceiverDonations = () => {
   const [collectors, setCollectors] = useState([]);
   const [tableReceipt, setTableReceipt] = useState([]);
   const [typeAlert, setTypeAlert] = useState("");
-  const [isEqual, setIsEqual] = useState(true);
+
+  const { receiveDonation, modalOpen, setModalOpen, modalConfig } = useDonation();
 
   const handleReceiverDonations = async (e) => {
     e.preventDefault();
-    const verify = await verifyCollector(collector, search, setMessage, setSearch);
 
-    if (verify) {
-      receiveDonation(
-        modifiedDate,
-        setMessage,
-        collector,
-        setTypeAlert,
-        search,
-        setTableReceipt
-      );
-      setSearch("")
-    }
-    if (verify === false){
-      console.log(verify);
-      setIsEqual(verify);
-    }
-  };
-
-  const btnYes = () => {
     receiveDonation(
       modifiedDate,
       setMessage,
@@ -50,13 +34,9 @@ const ReceiverDonations = () => {
       search,
       setTableReceipt
     );
-    setSearch("")
-    setIsEqual(true);
+    setSearch("");
   };
-  const btnNo = () => {
-    setIsEqual(true);
-    return null;
-  };
+
   useEffect(() => {
     setTableReceipt([]);
   }, [collector]);
@@ -81,7 +61,6 @@ const ReceiverDonations = () => {
           </h2>
         </div>
       </div>
-
       <form
         onSubmit={handleReceiverDonations}
         className="receiver-donations-form"
@@ -90,6 +69,7 @@ const ReceiverDonations = () => {
           <label className="label">Coletador</label>
           <select
             value={collector}
+            disabled={modalOpen}
             onChange={(e) => setCollector(e.target.value)}
           >
             <option value="" disabled>
@@ -108,7 +88,7 @@ const ReceiverDonations = () => {
 
         <div className="receiver-donations-form-input">
           <label className="label">Data</label>
-          <input type="date" value={date} onChange={handleDate} />
+          <input type="date" value={date} onChange={handleDate} readOnly={modalOpen}/>
         </div>
 
         <div className="receiver-donations-form-input">
@@ -117,34 +97,12 @@ const ReceiverDonations = () => {
             type="text"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            readOnly={!isEqual}
+            readOnly={modalOpen}
           />
         </div>
       </form>
-
       {/*MENSAGEM*/}
       <div className="receiver-donations-form-message">
-        {!isEqual && (
-          <div
-            style={{ backgroundColor: "#940000", width: "100%" }}
-            className="receiver-donations-form-message"
-          >
-            <p className="receiver-donations-form-message-text">
-              Coletador é diferente do recibo.
-            </p>
-            <p className="receiver-donations-form-message-text">
-              Deseja dar baixa mesmo assim?
-            </p>
-            <div className="btns">
-              <button onClick={btnYes} className="btn-decisionY">
-                Sim [Y]
-              </button>
-              <button onClick={btnNo} className="btn-decisionN">
-                Não [N]
-              </button>
-            </div>
-          </div>
-        )}
         {message && (
           <div
             style={{ backgroundColor: typeAlert, width: "100%" }}
@@ -159,7 +117,14 @@ const ReceiverDonations = () => {
           </div>
         )}
       </div>
-
+      {/*MODAL CONFIRM*/}
+      <ModalConfirm
+        isOpen={modalOpen}
+        onClose={() => setModalOpen(false)}
+        onConfirm={modalConfig.onConfirm}
+        title={modalConfig.title}
+        message={modalConfig.message}
+      />
       <table className="receiver-donations-table">
         {tableReceipt.length > 0 && (
           <thead className="receiver-donations-table-header">
