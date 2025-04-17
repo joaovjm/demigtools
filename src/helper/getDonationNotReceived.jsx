41c3065df9e2e9424d3bcb5437fe9f932ae40a83
joaovjm <joao.oliveira18.jm@gmail.com>
2025-04-17 17:24:31 -0300

    Fix

--- src/helper/getDonationNotReceived.jsx
+++ src/helper/getDonationNotReceived.jsx
@@ -26,22 +26,23 @@ const getDonationNotReceived = (
       )
       .eq("donation_received", "Não");
     
-    console.log(operatorValue)
     for (let i = 0; i < operatorValue.length; i++) {
       if (operatorValue[i].collector_code_id === 10) {
         confirmations += 1;
         let value = operatorValue[i].donation_value;
         valueConfirmations += value;
+
+        //Preenche o array com os doadores na confirmação
         tempDonationConfirmations.push({
           receipt_donation_id: operatorValue[i].receipt_donation_id,
           donor_name: operatorValue[i].donor.donor_name,
           donation_value: operatorValue[i].donation_value,
           collector_code_id: operatorValue[i].collector_code_id,
-          donor_confirmation_reason:
-            operatorValue[i].donor_confirmation_reason[0]
-              ?.donor_confirmation_reason,
+          donor_confirmation_reason: operatorValue[i].donor_confirmation_reason[0]?.donor_confirmation_reason,
         });
       }
+
+      //Preenche o array com todos os doadores
       tempFullNotReceivedDonations.push({
         receipt_donation_id: operatorValue[i].receipt_donation_id,
         donor_name: operatorValue[i].donor.donor_name,
@@ -54,6 +55,7 @@ const getDonationNotReceived = (
       let value = operatorValue[i].donation_value;
       valueOpenDonations += value;
     }
+
     setDonationConfirmation(tempDonationConfirmations);
     setFullNotReceivedDonations(tempFullNotReceivedDonations);
     setOpenDonations(operatorValue.length);
