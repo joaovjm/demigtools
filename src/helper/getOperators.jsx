af088c3fd3407260254dc6f6902d7c78f8300338
joaovjm <joao.oliveira18.jm@gmail.com>
2025-06-10 18:43:44 -0300

    Modificado para suportar a requisição

--- src/helper/getOperators.jsx
+++ src/helper/getOperators.jsx
@@ -1,7 +1,16 @@
 import supabase from "./superBaseClient"
 
-export const getOperators = async () => {
-    const {data, error} = await supabase.from("operator").select()
+export const getOperators = async (active, item) => {
+    let query = supabase.from("operator")
+    
+    if (item){
+        query = query.select(item)
+    } else {
+        query = query.select()
+    }
+    if (active) query = query.eq("operator_active", active)
+
+    const {data, error} = await query;
 
     if (data){
         return data
