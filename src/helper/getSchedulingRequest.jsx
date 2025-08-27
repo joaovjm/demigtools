9f16838a73134481da478502c55fab29fa96bfec
joaovjm <joao.oliveira18.jm@gmail.com>
2025-08-27 13:25:30 -0300

    Modificado a ordem da busca. Agora baseando-se no mes referente.

---
+++ src/helper/getSchedulingRequest.jsx
@@ -0,0 +1,14 @@
+import supabase from "./superBaseClient";
+
+export const getSchedulingRequest = async ({operatorID, }) => {
+  const { data, error } = await supabase
+    .from("request")
+    .select(
+      "donor_id, donor: donor_id(donor_name), request_scheduled_date, request_observation"
+    )
+    .eq("operator_code_id", operatorID)
+    if(error) throw error
+    if(data.length > 0) {
+        console.log(data)
+    }
+};
