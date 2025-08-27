b3129caefba488084c0de9fc7c23c2c6c2a64f7b
joaovjm <joao.oliveira18.jm@gmail.com>
2025-08-27 03:35:50 -0300

    Corrigido Problema no modal

--- src/components/ModalNewOperator/index.jsx
+++ src/components/ModalNewOperator/index.jsx
@@ -6,107 +6,112 @@ import { BtnNewOperator } from "../buttons/ActionButtons";
 import UsersToOperators from "../../auth/UsersToOperators";
 import { toast, ToastContainer } from "react-toastify";
 
+const ModalNewOperator = ({ setModalShow, setStatus }) => {
+  const typeOperator = ["Admin", "Operador", "Mensal"];
+  const [newOperator, setNewOperator] = useState({
+    cod: "",
+    operator: "",
+    password: "",
+    type: "",
+  });
 
-const ModalNewOperator = ({setModalShow, setStatus}) => {
-    const typeOperator = ["Admin", "Operador", "Mensal"]
-    const [newOperator, setNewOperator] = useState({
-        cod: "",
-        operator: "",
-        password: "",
-        type: ""
-    })
+  const handleOperatorChange = (e) => {
+    const { name, value } = e.target;
+    setNewOperator((prev) => ({ ...prev, [name]: value }));
+  };
 
-    const handleOperatorChange = (e) => {
-        const {name, value} = e.target
-        setNewOperator((prev) => ({...prev, [name]: value}))
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if (
+      !newOperator.cod ||
+      !newOperator.operator ||
+      !newOperator.password ||
+      !newOperator.type
+    ) {
+      toast.warning("Preencha todos os campos!");
     }
-
-    const handleClick = async (e) => {
-        e.preventDefault();
-        if (!newOperator.cod || !newOperator.operator || !newOperator.password || !newOperator.type){
-          toast.warning("Preencha todos os campos!")
-        }
-        try{
-            const status = await UsersToOperators(newOperator)
-            if(status === "OK"){
-              toast.success("Operador criado com sucesso!")
-              setTimeout(()=> {
-                setModalShow(false)
-              }, 2000)
-              
-              
-            }
-            
-        } catch (error) {
-            console.error("Erro: ", error.message)
-        }
+    try {
+      const status = await UsersToOperators(newOperator);
+      if (status === "OK") {
+        toast.success("Operador criado com sucesso!");
+        setTimeout(() => {
+          setModalShow(false);
+        }, 2000);
+      }
+    } catch (error) {
+      console.error("Erro: ", error.message);
     }
+  };
 
   return (
-    <div className="modal-container">
-      <div className="modal">
-        <form className="modal-form">
-          <div className="modal-title">
+    <main className="modal-newoperator">
+      <div className="modal-newoperator-div">
+        <div className="modal-newoperator-form">
+          <div className="modal-newoperator-form-title">
             <h3 className="modal-title-text">Novo Operador</h3>
-            <button 
-              type="button" 
-              className="modal-close-button" 
+            <button
+              type="button"
+              className="modal-close-button"
               onClick={() => setModalShow(false)}
             >
               Fechar
             </button>
           </div>
-          <div className="inputs">
-            <FormInput
-              label="Codigo"
+
+          <div className="input-field">
+            <label>Codigo</label>
+            <input
               type="text"
               name="cod"
               value={newOperator.cod}
               onChange={handleOperatorChange}
-              style={{ width: 172 }}
             />
-            <FormInput
-              label="Nome"
+          </div>
+          <div className="input-field">
+            <label>Nome</label>
+            <input
               type="text"
               name="operator"
               value={newOperator.operator}
-              autoComplete="username"
               onChange={handleOperatorChange}
-              style={{ width: 182 }}
+              autoComplete="username"
             />
-            <FormInput
-              label="Senha"
+          </div>
+          <div className="input-field">
+            <label>Senha</label>
+            <input
               type="password"
               name="password"
               value={newOperator.password}
+              onChange={handleOperatorChange}
               autoComplete="new-password"
-              onChange={handleOperatorChange}
-              style={{ width: 182 }}
-            />
-            <FormListSelect
-            className="label"
-              label="Tipo"
-              name="type"
-              value={newOperator.type}
-              options={typeOperator}
-              ///disableOption={}
-              defaultValue="Admin"
-              onChange={handleOperatorChange}
-              style={{ width: 198 }}
             />
           </div>
 
+          <FormListSelect
+            className="label"
+            label="Tipo"
+            name="type"
+            value={newOperator.type}
+            options={typeOperator}
+            ///disableOption={}
+            defaultValue="Admin"
+            onChange={handleOperatorChange}
+            style={{ width: 198 }}
+          />
+
           <div className="btns">
-            <BtnNewOperator
-                className="btn-edit"
-                type="button"
-                onClick={handleClick}
-            />
+            <BtnNewOperator className="btn-edit" onClick={handleClick} />
           </div>
-        </form>
+        </div>
       </div>
-      <ToastContainer autoClose={2000} closeOnClick="true" pauseOnFocusLoss="false" position="top-left"/>
-    </div>
+      <ToastContainer
+        autoClose={2000}
+        closeOnClick="true"
+        pauseOnFocusLoss="false"
+        position="top-left"
+      />
+    </main>
   );
 };
 
