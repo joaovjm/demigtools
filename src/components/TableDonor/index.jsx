7dd2bd5bd6704c1e4e4d1fb4f30a30a0af3e68b5
joaovjm <joao.oliveira18.jm@gmail.com>
2025-08-28 17:09:01 -0300

    Agora a tabela desaparece caso não tenha nenhum movimento.

--- src/components/TableDonor/index.jsx
+++ src/components/TableDonor/index.jsx
@@ -46,96 +46,102 @@ const TableDonor = ({
 
   return (
     <div className="table-wrapper">
-      <table className="tabledonor">
-        <thead>
-          <tr className="trHead">
-            <th className="tableHead">Recibo</th>
-            <th className="tableHead">Operador</th>
-            <th className="tableHead">Valor</th>
-            <th className="tableHead">Extra</th>
-            <th className="tableHead">Contato</th>
-            <th className="tableHead">Receber</th>
-            <th className="tableHead">Recebida</th>
-            <th className="tableHead">Impresso</th>
-            <th className="tableHead">Recebido</th>
-            <th className="tableHead">MesRef</th>
-            <th className="tableHead">Coletador</th>
-          </tr>
-        </thead>
+      {dados.length > 0 ? (
+        <>
+          <table className="tabledonor">
+            <thead>
+              <tr className="trHead">
+                <th className="tableHead">Recibo</th>
+                <th className="tableHead">Operador</th>
+                <th className="tableHead">Valor</th>
+                <th className="tableHead">Extra</th>
+                <th className="tableHead">Contato</th>
+                <th className="tableHead">Receber</th>
+                <th className="tableHead">Recebida</th>
+                <th className="tableHead">Impresso</th>
+                <th className="tableHead">Recebido</th>
+                <th className="tableHead">MesRef</th>
+                <th className="tableHead">Coletador</th>
+              </tr>
+            </thead>
 
-        <tbody>
-          {dados && dados.length > 0 ? (
-            dados.map((item) => (
-              <Fragment key={item.receipt_donation_id}>
-                <tr onClick={() => handleEditDonation(item)} className="trBody">
-                  <td className="tableBody">{item.receipt_donation_id}</td>
-                  <td className="tableBody">
-                    {item.operator_code_id} - {item.operator?.operator_name}
-                  </td>
-                  <td className="tableBody">
-                    {item.donation_value.toLocaleString("pt-BR", {
-                      style: "currency",
-                      currency: "BRL",
-                    })}
-                  </td>
-                  <td className="tableBody">
-                    {item?.donation_extra?.toLocaleString("pt-BR", {
-                      style: "currency",
-                      currency: "BRL",
-                    })}
-                  </td>
-                  <td className="tableBody">
-                    {new Date(item.donation_day_contact).toLocaleDateString(
-                      "pt-BR"
-                    )}
-                  </td>
-                  <td className="tableBody">
-                    {new Date(item.donation_day_to_receive).toLocaleDateString(
-                      "pt-BR"
-                    )}
-                  </td>
-                  <td className="tableBody">
-                    {item.donation_day_received
-                      ? `${new Date(
-                          item?.donation_day_received
-                        ).toLocaleDateString("pt-BR")}`
-                      : ""}
-                  </td>
-                  <td className="tableBody">{item.donation_print}</td>
-                  <td className="tableBody">{item.donation_received}</td>
-                  <td className="tableBody">
-                    {item.donation_monthred
-                      ? `${new Date(item?.donation_monthref).toLocaleDateString(
-                          "pt-BR"
-                        )}`
-                      : ""}
-                  </td>
-                  <td className="tableBody">
-                    {item.collector_code_id} -{" "}
-                    {item.collector?.collector_name || ""}
-                  </td>
-                </tr>
-                <tr className="trFoot">
-                  <td colSpan="5" className="obs">
-                    {item.donation_description
-                      ? item.donation_description
-                      : "..."}
-                  </td>
-                  <td colSpan="6" className="obs">
-                    Campanha: {item.donation_campain}
-                  </td>
-                </tr>
-              </Fragment>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="11" className="tableBody no-data">
-                Não há dados de doação disponíveis
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+            <tbody>
+              {dados && dados.length > 0
+                ? dados.map((item) => (
+                    <Fragment key={item.receipt_donation_id}>
+                      <tr
+                        onClick={() => handleEditDonation(item)}
+                        className="trBody"
+                      >
+                        <td className="tableBody">
+                          {item.receipt_donation_id}
+                        </td>
+                        <td className="tableBody">
+                          {item.operator_code_id} -{" "}
+                          {item.operator?.operator_name}
+                        </td>
+                        <td className="tableBody">
+                          {item.donation_value.toLocaleString("pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                          })}
+                        </td>
+                        <td className="tableBody">
+                          {item?.donation_extra?.toLocaleString("pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                          })}
+                        </td>
+                        <td className="tableBody">
+                          {new Date(
+                            item.donation_day_contact
+                          ).toLocaleDateString("pt-BR")}
+                        </td>
+                        <td className="tableBody">
+                          {new Date(
+                            item.donation_day_to_receive
+                          ).toLocaleDateString("pt-BR")}
+                        </td>
+                        <td className="tableBody">
+                          {item.donation_day_received
+                            ? `${new Date(
+                                item?.donation_day_received
+                              ).toLocaleDateString("pt-BR")}`
+                            : ""}
+                        </td>
+                        <td className="tableBody">{item.donation_print}</td>
+                        <td className="tableBody">{item.donation_received}</td>
+                        <td className="tableBody">
+                          {item.donation_monthred
+                            ? `${new Date(
+                                item?.donation_monthref
+                              ).toLocaleDateString("pt-BR")}`
+                            : ""}
+                        </td>
+                        <td className="tableBody">
+                          {item.collector_code_id} -{" "}
+                          {item.collector?.collector_name || ""}
+                        </td>
+                      </tr>
+                      <tr className="trFoot">
+                        <td colSpan="5" className="obs">
+                          {item.donation_description
+                            ? item.donation_description
+                            : "..."}
+                        </td>
+                        <td colSpan="6" className="obs">
+                          Campanha: {item.donation_campain}
+                        </td>
+                      </tr>
+                    </Fragment>
+                  ))
+                : null}
+            </tbody>
+          </table>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
