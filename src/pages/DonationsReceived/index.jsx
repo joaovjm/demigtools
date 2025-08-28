e9c0fc210f5cfafca7e9b9038ec74ebbe9e81854
joaovjm <joao.oliveira18.jm@gmail.com>
2025-08-28 03:30:55 -0300

    Finalizado a pagina de relatorio de doação recebida.

--- src/pages/DonationsReceived/index.jsx
+++ src/pages/DonationsReceived/index.jsx
@@ -2,24 +2,29 @@ import React, { useEffect, useState } from "react";
 import "./index.css";
 import supabase from "../../helper/superBaseClient";
 import { DataSelect } from "../../components/DataTime";
+import Loader from "../../components/Loader";
 
 const DonationsReceived = () => {
   const [startDate, setStartDate] = useState();
   const [endDate, setEndDate] = useState();
   const [donationReceived, setDonationReceived] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalCount, setTotalCount] = useState();
+  const [totalValue, setTotalValue] = useState();
 
   const fetchDonationReceived = async (i) => {
     let dateAdd;
+
 
     if (i >= 0) {
       const newDate = new Date(startDate);
       newDate.setDate(newDate.getDate() + i);
       const newDateAdd = newDate;
       dateAdd = DataSelect(newDateAdd, "noformated");
-      
+
     } else {
       dateAdd = startDate;
-      console.log("Chegou aqui");
+
     }
     try {
       const { data, error } = await supabase
@@ -34,6 +39,7 @@ const DonationsReceived = () => {
           0
         );
         const count = data.length;
+        
         return { valueDonation, count, dateAdd };
       }
     } catch (error) {
@@ -41,21 +47,33 @@ const DonationsReceived = () => {
     }
   };
 
-  console.log(donationReceived)
-
   const handleDonationReceived = async () => {
+    setIsLoading(true);
     setDonationReceived([]);
+    let totalCount = 0;
+    let totalValue = 0;
     const diffTime = Math.abs(new Date(endDate) - new Date(startDate));
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
     if (diffDays === 0) {
       const { valueDonation, count, dateAdd } = await fetchDonationReceived();
-      setDonationReceived({ valueDonation, count, dateAdd });
+      setDonationReceived([{ valueDonation, count, dateAdd }]);
     } else {
       for (let i = 0; i <= diffDays; i++) {
-        const { valueDonation, count, dateAdd } = await fetchDonationReceived(i);
-        setDonationReceived((prev) => [...prev, { valueDonation, count, dateAdd }]);
+        const { valueDonation, count, dateAdd } = await fetchDonationReceived(
+          i
+        );
+        setDonationReceived((prev) => [
+          ...prev,
+          { valueDonation, count, dateAdd },
+        ]);
+
+        totalValue = totalValue + valueDonation;
+        totalCount = totalCount + count
       }
     }
+    setTotalValue(totalValue);
+    setTotalCount(totalCount)
+    setIsLoading(false);
   };
 
   return (
@@ -77,34 +95,44 @@ const DonationsReceived = () => {
             onChange={(e) => setEndDate(e.target.value)}
           />
         </div>
-        <button onClick={handleDonationReceived}>Gerar</button>
+        <button style={{ width: 80 }} onClick={handleDonationReceived}>
+          {isLoading ? <Loader /> : "Gerar"}
+        </button>
       </div>
       <div className="donation-received-container-body">
-        <table>
-          <thead>
-            <tr>
-              <th>Dia</th>
-              <th>Fichas</th>
-              <th>Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {donationReceived?.map((item) => (
-              <tr>
-                <td>
-                  {new Date(item.dateAdd).toLocaleDateString("pt-BR", {timeZone: 'UTC'}) || null}
-                </td>
-                <td>{item.count || 0}</td>
-                <td>
-                  {item.valueDonation?.toLocaleString("pt-BR", {
-                    style: "currency",
-                    currency: "BRL",
-                  }) || 0}
-                </td>
+        {donationReceived.length > 0 ? (
+          <table className="donation-received-table">
+            <thead>
+              <tr className="donation-received-table-tr">
+                <th>Dia</th>
+                <th>Fichas</th>
+                <th>Total</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {donationReceived?.map((item) => (
+                <tr className="donation-received-table-body-tr">
+                  <td>
+                    {new Date(item.dateAdd).toLocaleDateString("pt-BR", {
+                      timeZone: "UTC",
+                    }) || null}
+                  </td>
+                  <td>{item.count || 0}</td>
+                  <td>
+                    {item.valueDonation?.toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    }) || 0}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : null}
+      </div>
+      <div className="donation-received-result">
+        <label>Fichas: {totalCount}</label>
+        <label>Total Geral: {totalValue.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</label>
       </div>
     </div>
   );
