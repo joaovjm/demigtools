5b1c3c8217be6cda2614fed8c4133e73ad279b72
joaovjm <joao.oliveira18.jm@gmail.com>
2025-09-10 13:19:32 -0300

    Atualização da geração do recibo

--- src/components/modals/ModalReceiptSend/index.jsx
+++ src/components/modals/ModalReceiptSend/index.jsx
@@ -2,25 +2,31 @@ import React, { useState } from "react";
 import "./index.css";
 import { FaDollarSign } from "react-icons/fa6";
 const ModalReceiptSend = ({ setSendModalOpen, deposit, setDeposit }) => {
-  console.log(deposit);
   const [whatsapp, setWhatsapp] = useState();
-  const [itemClicked, setItemClicked] = useState()
+  const [inEdit, setInEdit] = useState("");
 
   const handleEdit = (item, index) => {
-    if (itemClicked){
-        setDeposit(deposit.map((dp) => {
-            if (dp.receipt_donation_id === item.receipt_donation_id){
-                return {...dp, donor: {...dp.donor, donor_tel_1: whatsapp} }
-            }
-            return dp;
-        }))
-        setItemClicked()
+    if (inEdit === item.receipt_donation_id) {
+      setInEdit("");
     } else {
-        setWhatsapp(item.donor?.donor_tel_1)
-        setItemClicked(item.receipt_donation_id)
+      setInEdit(item.receipt_donation_id);
     }
-    
-  }
+
+    /*if (itemClicked) {
+      setDeposit(
+        deposit.map((dp) => {
+          if (dp.receipt_donation_id === item.receipt_donation_id) {
+            return { ...dp, donor: { ...dp.donor, donor_tel_1: whatsapp } };
+          }
+          return dp;
+        })
+      );
+      setItemClicked();
+    } else {
+      setWhatsapp(item.donor?.donor_tel_1);
+      setItemClicked(item.receipt_donation_id);
+    }*/
+  };
   return (
     <div className="modal-area">
       <div className="modal-send-container">
@@ -42,37 +48,36 @@ const ModalReceiptSend = ({ setSendModalOpen, deposit, setDeposit }) => {
               key={item.receipt_donation_id}
               className="modal-send-body-receipt"
             >
-              <label>{item.receipt_donation_id}</label>
-              <label>{item.donor?.donor_name}</label>
-              <label>{item.donor?.donor_tel_1}</label>
+              <div className="input-field">
+                <label>Recibo</label>
+                <p>{item.receipt_donation_id}</p>
+              </div>
+              <div className="input-field">
+                <label>Nome</label>
+                <p style={{ width: "300px" }}>{item.donor?.donor_name}</p>
+              </div>
+
+              <div className="input-field">
+                <label>Whatsapp</label>
+                <input
+                  type="text"
+                  style={{ width: "160px" }}
+                  value={item.donor.donor_tel_1}
+                  onChange={(e) => setWhatsapp(e.target.value)}
+                  readOnly={inEdit !== item.receipt_donation_id}
+                />
+              </div>
               <div className="modal-send-body-receipt-btn">
                 <button
                   onClick={() => handleEdit(item, index)}
                   className="modal-send-body-receipt-btn-edit"
                 >
-                  {itemClicked === item.receipt_donation_id ? "Salvar" : "Editar"}
+                  {inEdit === item.receipt_donation_id ? "Salvar" : "Editar"}
                 </button>
                 <button className="modal-send-body-receipt-btn-send">
                   Enviar
                 </button>
               </div>
-              {itemClicked === item.receipt_donation_id && (
-                <div className="modal-send-body-receipt-edit">
-                  <div className="input-field">
-                    <label>Mensagem</label>
-                    <textarea style={{ padding: "6px" }} />
-                  </div>
-                  <div className="input-field">
-                    <label>Whatsapp</label>
-                    <input
-                      type="text"
-                      style={{ padding: "6px" }}
-                      value={whatsapp}
-                      onChange={(e) => setWhatsapp(e.target.value)}
-                    />
-                  </div>
-                </div>
-              )}
             </div>
           ))}
         </div>
