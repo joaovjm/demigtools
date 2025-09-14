28862b23bcc1381e2c8362dbcb47aaf3cd438b4e
joaovjm <joao.oliveira18.jm@gmail.com>
2025-09-14 00:22:14 -0300

    Adicição do servidor de email

---
+++ src/api/send-email.js
@@ -0,0 +1,56 @@
+import nodemailer from "nodemailer";
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "10mb",
+    }
+  }
+}
+
+export default async function handler(req, res) {
+
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "http://demigtools.vercel.app",
+  ]
+
+  const origin = req.headers.origin;
+
+  if (!allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+
+  if (req.method === "OPTIONS") {
+
+    return res.status(200).end();
+  }
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Método não permitido" });
+  }
+
+  const { emailTo, subject, text } = req.body;
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+  const options = {
+    from: `"João Oliveira" <${process.env.EMAIL_USER}>`,
+    to: emailTo,
+    subject: subject,
+    text: text,
+  };
+  try {
+    await transporter.sendMail(options);
+    res.status(200).json({ message: "Email enviado com sucesso" });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao enviar email" });
+  }
+}
