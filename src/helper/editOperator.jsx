dd464ff390c9df4e5200da671cf332ae9276bf2c
joaovjm <joao.oliveira18.jm@gmail.com>
2025-11-04 12:07:09 -0300

    Corrigido problema que impedia de atualizar o tipo de operador.

--- src/helper/editOperator.jsx
+++ src/helper/editOperator.jsx
@@ -2,15 +2,12 @@ import supabase from "./superBaseClient"
 
 const editOperator = async ({ id, name, type, active, password }) => {
     try{
-        const { data, error } = await supabase.from("operator").update([
-            {
-                operator_code_id: id, 
-                operator_name: name,
-                operator_type: type,
-                operator_active: active,
-                operator_password: password
-            }
-        ])
+        const { data, error } = await supabase.from("operator").update({
+            operator_name: name,
+            operator_type: type,
+            operator_active: active,
+            operator_password: password
+        })
         .eq("operator_code_id", id)
         .select();
 
