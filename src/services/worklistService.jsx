db4efca32913d4d5c9be1a734237ab90329e9b77
joaovjm <joao.oliveira18.jm@gmail.com>
2026-01-08 11:58:06 -0300

    Enable VOIP feature and enhance worklist fetching logic
    - Updated voipConfig.json to activate the VOIP feature.
    - Refactored fetchWorklist function in worklistService.jsx to include a date filter for active requests, improving data retrieval accuracy.
    - Added error handling for the fetchWorklist function to ensure robust logging of issues.

--- src/services/worklistService.jsx
+++ src/services/worklistService.jsx
@@ -1,26 +1,32 @@
 import { getMaxAndMedDonations } from "../helper/getMaxAndMedDonations";
 import getWorklistRequests from "../helper/getWorklistRequests";
 import supabase from "../helper/superBaseClient";
+import { DataNow } from "../components/DataTime";
 
 export async function fetchWorklist() {
-  const { data, error } = await supabase
-    .from("request")
-    .select()
-    .eq("request_active", "True");
-  if (error) console.error(error.message);
+  try {
+    const { data, error } = await supabase
+      .from("request")
+      .select()
+      .eq("request_active", "True")
+      .gte("request_end_date", DataNow("noformated"));
+    if (error) console.error(error.message);
 
-  if (data.length > 0) {
-    const worklist = [
-      ...new Map(
-        data.map((dt) => [
-          dt.request_name,
-          {
-            name: dt.request_name,
-          },
-        ])
-      ).values(),
-    ];
-    return worklist;
+    if (data.length > 0) {
+      const worklist = [
+        ...new Map(
+          data.map((dt) => [
+            dt.request_name,
+            {
+              name: dt.request_name,
+            },
+          ])
+        ).values(),
+      ];
+      return worklist;
+    }
+  } catch (error) {
+    console.error(error.message);
   }
 }
 
@@ -30,6 +36,18 @@ export async function worklistRequests(operatorID, workSelect) {
 }
 
 export async function fetchMaxAndMedDonations(id, requestName) {
-  const { maxGeneral, maxPeriod, penultimate, countNotReceived, lastThreeDonations } = await getMaxAndMedDonations(id, requestName);
-  return { maxGeneral, maxPeriod, penultimate, countNotReceived, lastThreeDonations };
+  const {
+    maxGeneral,
+    maxPeriod,
+    penultimate,
+    countNotReceived,
+    lastThreeDonations,
+  } = await getMaxAndMedDonations(id, requestName);
+  return {
+    maxGeneral,
+    maxPeriod,
+    penultimate,
+    countNotReceived,
+    lastThreeDonations,
+  };
 }
